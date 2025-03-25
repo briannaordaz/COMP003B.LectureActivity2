5cafbd8d9b842e906d9cb8b2f051bc3321f4fe7b
briannaordaz <briannaordaz5@gmail.com>
2025-03-24 18:35:19 -0700

    Added author, course, faculty, and purpose,Configured Middleware in Program.cs, and  Created custom Middleware

---
+++ COMP003B.LectureActivity2/Program.cs
@@ -0,0 +1,43 @@
+// Author: Brianna Ordaz
+// Course: COMP003B
+// Faculty: Jonathan Cruz
+// Purpose: Implement built-in middleware
+
+namespace COMP003B.LectureActivity2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        // Middleware sequence
+        app.UseHttpsRedirection();
+        
+        app.UseStaticFiles();
+
+        app.UseMiddleware<COMP003B.LectureActivity2.Middleware.RequestLoggingMiddleware>();
+        
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
