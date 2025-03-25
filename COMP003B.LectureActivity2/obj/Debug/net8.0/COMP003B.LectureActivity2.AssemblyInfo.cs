abe5212310f56a13cffc4b604d57a50be7a514a6
briannaordaz <briannaordaz5@gmail.com>
2025-03-24 19:04:26 -0700

    added a Partial View: Sidebar

--- COMP003B.LectureActivity2/obj/Debug/net8.0/COMP003B.LectureActivity2.AssemblyInfo.cs
+++ COMP003B.LectureActivity2/obj/Debug/net8.0/COMP003B.LectureActivity2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP003B.LectureActivity2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e009968e2de132cf00b2f83afb3ad84b53b76b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP003B.LectureActivity2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP003B.LectureActivity2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
